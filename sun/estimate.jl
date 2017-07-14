3b32496cf85b01a1812e24fb3c788a63bfc35407
Adrian Roth <adrianroth10@gmail.com>
2017-07-14 13:07:32 +0200

    estimation of the sunsets and sunrises

---
+++ sun/estimate.jl
@@ -0,0 +1,14 @@
+data, header = readdlm(ARGS[1], '\t', String, '\n', header=true)
+
+dates = map(x -> DateTime(x, "mm-dd"), data[:, 1])
+dates = map(Dates.datetime2epochms, dates) / 1000 / 3600 / 24 - 365
+dates = reshape(dates, length(dates), 1)
+times = map(x -> DateTime(x, "HH-MM"), data[:, 2:3])
+times = map(Dates.datetime2epochms, times) / 1000 - 3.1624776e7
+sunrise = times[:, 1:1]
+sunset = times[:, 2:2]
+
+X = hcat(ones(length(dates)), dates, dates.^2)
+a1 = (X' * X) \ X' * sunrise
+a2 = (X' * X) \ X' * sunset
+writedlm(string(ARGS[1], "_out"), vcat(a1, a2))
---
+++ sun/estimate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+julia estimate.jl la_jument
+julia estimate.jl simpnasklubb
